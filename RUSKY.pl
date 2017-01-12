5ca047c068fa2b99bf8a4e95e69eeece6ca38d03
Prince of Archives <rich.tijerina@gmail.com>
2017-01-12 01:27:26 -0700

    Update RUSKY.pl

--- RUSKY.pl
+++ RUSKY.pl
@@ -1,12 +1,13 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
+use Proc::Daemon;
 use LWP::UserAgent;
 use WWW::Mechanize;
 use HTTP::Cookies;
-#################################
+use MKRX::XS;
+######################################
 # RUSKY - scrape golibgen.io pdfs
-
-# DAEMONIZE ##############################
+# DAEMONIZE ##########################
 $daemon = Proc::Daemon->new(
     work_dir     => '/MINION/RUSKY',
     child_STDOUT => 'RUSKY_log',
@@ -14,7 +15,16 @@ $daemon = Proc::Daemon->new(
     pid_file     => 'RUSKY_pid',
     exec_command => 'perl RUSKY.pl',
 );
-# USER AGENT ####################
+# SETUP ##############################
+my $base = "http://golibgen.io/view.php?id=";
+my $dump = "RUSKY_dump";
+my $pool = "RUSKY_pool";
+my $g = "RUSKY_g";
+my $inital = "RUSKY_init";
+# INITIALIZE #########################
+open(my $ifh, '<', $inital) or die "cant open $inital";
+my $ttl = readline $ifh; chomp $ttl; close $ifh;
+# USER AGENT #########################
 my $ua = LWP::UserAgent->new();
 my $cookies = HTTP::Cookies->new(
 	file => "cookies.txt",
@@ -22,12 +32,18 @@ my $cookies = HTTP::Cookies->new(
 );
 $ua->cookie_jar($cookies);
 $ua->agent("Windows IE 7");
-
-my $base = "http://golibgen.io/view.php?id=";
-my $mech = WWW::Mechanize->new($ua);
-my $tst = 23;
-my $url = $base.$tst;
-print "$url\n";
-$mech->get($url);
-$mech->click('submit');
-$mech->save_content("23");
+# MECH ###############################
+while ($ttl > 0) { 
+	my $iter = $ttl;
+	my $mech = WWW::Mechanize->new($ua);
+	my $url = $base.$iter;
+    print "scraping $url\n";
+	$mech->get($url);
+	$mech->click('submit');
+	$mech->save_content("$dump/$iter");
+	XS($dump $pool $g) or die "can't XS $dump/$iter";
+	$ttl--;
+	open(my $finfh, '>', $initial) or die "can't reopen $inital";
+	print $finfh "$ttl\n";
+	print "stored $url\n";
+}
