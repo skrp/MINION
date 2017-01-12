b494553328a9af45374a4bc9a52e91e23d5b1eac
Prince of Archives <rich.tijerina@gmail.com>
2017-01-12 00:29:58 -0700

    Update IGOR.pl

--- IGOR.pl
+++ IGOR.pl
@@ -1,6 +1,18 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
 use Proc::Daemon;
-###########################
+##########################################
 # IGOR - minion slavemaster
-
+my @cmds = qw(pause stat errchk dive countoff);
+# DAEMONIZE ##############################
+$daemon = Proc::Daemon->new(
+    work_dir     => '/MINION/IGOR',
+    child_STDOUT => 'IGOR_log',
+    child_STDERR => '+>>IGOR_debug',
+    pid_file     => 'IGOR_pid',
+    exec_command => 'perl IGOR.pl',
+);
+# SUB ####################################
+sub pause {
+  my ($minion) = @_;
+}
