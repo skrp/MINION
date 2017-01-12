788f20401a621ebc749f0dcb8c00a36a45f84857
Prince of Archives <rich.tijerina@gmail.com>
2017-01-11 23:49:09 -0700

    Create ARKI.pl

---
+++ ARKI.pl
@@ -0,0 +1,46 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+use LWP::UserAgent;
+use HTTP::Cookies;
+use File::Find::Rule;
+###########################################
+# ARKI - scrape archive.org pdfs ##########
+###########################################
+my ($target, $dump) = @ARGV;
+die "not a target dir" unless -d $target;
+die "not a dump dir" unless -d $dump;
+my $file = 'tmp';
+my $base = "http://archive.org/download";
+# USER AGENT #############################
+my $ua = LWP::UserAgent->new();
+my $cookies = HTTP::Cookies->new(
+	file=>"$dump/cookies.txt",
+	autosave => 1,
+);
+$ua->cookie_jar($cookies);
+$ua->agent("Windows IE 7");
+$ua->timeout(60);
+# BATCH PROC #############################
+my $rule = File::Find::Rule->file()->start($target);
+while (defined(my $file = $rule->match)){
+	open(my $ifh, '<', $file) or die "Couldn't read $file\n";
+	my @list = readline $ifh; chomp @list;
+	foreach my $i (@list) {
+# PAUSE ##################################
+		if (-e "/tmp/PGET_PAUSE")
+			{ pause; }
+		print $lfh "$i  started\n"; # LOG #########
+		my $url = "$base/$i/$i.pdf";7
+		my $response = $ua->get($url, ':content_file'=>"$dump/$i");
+    my $url = "$base/$i/$i.pdf";7
+		my $response = $ua->get($url, ':content_file'=>"$dump/$i");
+    my $murl = "$base"/"$i"_meta.xml;
+    my $mresponse = $ua->get($url, ':content_file'=>"$dump"/"$i"_meta.xml);
+		print "$i  ended\n";
+	}
+	close $ifh;
+	unlink $file or die "Cant delete after use: $file";
+} 
+# SUB ###################################
+sub pause 
+	{ sleep(500); }	
