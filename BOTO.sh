11a4cc2fc4cac74d008f3274331091eb97c168c5
Prince of Archives <rich.tijerina@gmail.com>
2017-01-12 23:30:29 -0700

    Create BOTO.sh

---
+++ BOTO.sh
@@ -0,0 +1,39 @@
+#!/usr/local/bin/bash
+###############################
+# BOTO - website mirror scraper
+# -----------------skrp of MKRX
+# FUNCTION ####################
+scrape () {
+sub="$1"
+wget -e robots=off --no-check-certificate --mirror \
+--convert-links --continue $sub \
+|| printf "%s failed\n" "$sub" >> BOTO_log;
+}
+init () {
+index=0
+while read -r line
+do
+	TASKLIST[$index]=$line;
+	((index++));
+done < BOTO_target 
+rm BOTO_target;
+}
+task () {
+for i in "${TASKLIST[@]}"
+do
+	sub="$i"
+	scrape "$sub";
+done
+}
+# ACTION  #######################
+while true
+do
+	if [ -f BOTO_target ] 
+	then
+		init;
+		task;
+	else
+		sleep 1000;
+	fi
+done
+
