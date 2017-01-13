f2b0b3b9cf13fae283cf1508dae181d69502f817
Prince of Archives <rich.tijerina@gmail.com>
2017-01-12 23:27:31 -0700

    Create lil_NEO.sh

---
+++ lil_NEO.sh
@@ -0,0 +1,15 @@
+#!/usr/local/bin/bash
+#############################
+# NEO - scrape searchcode.com
+# -------------- skrp of MKRX
+# BEGIN #####################
+init=$( cat init )
+base="http://searchcode.com/codesearch/raw/";
+while [ $init -lt 127100000 ]
+do
+	((init++));
+	fetch $base$init || printf "%s fail\n" "$init" >> log;
+	echo $init > init;
+	printf  "%s done\n" "$init";
+done
+
