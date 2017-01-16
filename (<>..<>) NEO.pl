8700318091bed8758d0451e401df4f370b0e89f7
Prince of Archives <rich.tijerina@gmail.com>
2017-01-16 03:57:46 -0700

    Update (<>..<>) NEO.pl

--- (<>..<>) NEO.pl
+++ (<>..<>) NEO.pl
@@ -10,8 +10,9 @@ my ($dump) = @ARGV; $dump =~ s%/\z%%;
 die "not a dump dir" unless -d $dump;
 # SET UP ####################
 my $base = "https://searchcode.com/codesearch/raw/";
-my $init = "init";
+my $init = "init"; my $log = "log";
 open(my $ifh, '<', $init) or die "Couldn't read $init\n";
+open(my $lfh, '>>', $log) or die "Couldn't read $log\n";
 my $point = readline $ifh; chomp $point; close $ifh;
 # LOOP ######################
 while ($point < 127100000) {
@@ -23,7 +24,8 @@ while ($point < 127100000) {
 		$mech->save_content("$dump/$point");
 		print "$point  ended\n";
 	}
-	else { next; }
+	else 
+		{ print $lfh "$point fail\n"; next; }
 # ACCOUNTING ################
 	if ($point % 100 == 0) {
 		open(my $fifh, '>', $init);
