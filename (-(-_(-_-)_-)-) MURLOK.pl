8617faa851793bdc222f5b6d1a50934c6f326ffa
Prince of Archives <rich.tijerina@gmail.com>
2017-01-21 04:51:14 -0700

    Create (-(-_(-_-)_-)-) MURLOK.pl

---
+++ (-(-_(-_-)_-)-) MURLOK.pl
@@ -0,0 +1,6 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+use Proc::Daemon;
+#######################################
+# MURLOCKS - amphibious listen & croak 
+$ (-(-_(-_-)_-)-)      ---skrp of MKRX
