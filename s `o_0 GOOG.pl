9c57ee98ac667c5b94143053fbaaedef12f6bf3f
Prince of Archives <rich.tijerina@gmail.com>
2017-02-04 01:18:40 -0700

    Update and rename `o_0 GOOG.pl to s `o_0 GOOG.pl

--- s `o_0 GOOG.pl
+++ s `o_0 GOOG.pl
@@ -3,7 +3,7 @@ use strict; use warnings;
 use Proc::Daemon;
 use LWP;
 use WWW::Mechanize;
-############################
+################## SUMMONS #
 # GOOG - imgur viral scraper
 #    `o_0    ---skrp of MKRX
 # SETUP ####################
