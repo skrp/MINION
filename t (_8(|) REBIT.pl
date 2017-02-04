eeb318b580d2cd5e8da9d677b6cc6c07851dd7ec
Prince of Archives <rich.tijerina@gmail.com>
2017-02-04 01:32:34 -0700

    Update t (_8(|) REBIT.pl

--- t (_8(|) REBIT.pl
+++ t (_8(|) REBIT.pl
@@ -3,3 +3,24 @@
 # REBIT - iteration parser
 #   (_8(|) ---skrp of MKRX
 # - cur_iter - init_length - update_MASTER - cat_init
+my $dir = 'MINIONS/';
+my @list = readdir($dir);
+my @active;
+foreach my $minion (@list) {
+  if (defined REBIT) 
+    { push @active $minion; }
+}
+sub counter { # list $minion_dump 
+  my ($mininon) = @_;
+  my $minion_dump = "$minion".'_dump';
+  my @count = readdir($minion_dump);
+  my $count = @counter;
+  print "$count\n";
+}
+sub up_MASTER { # list $minion_dump 
+  my ($mininon) = @_;
+  my $minion_dump = "$minion".'_dump';
+  my @count = readdir($minion_dump);
+  my $counter = \@count;
+  return $counter;
+}
