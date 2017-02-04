d8145e5a1e7cd990f5c6fbd7ffd11567c5de4cf4
Prince of Archives <rich.tijerina@gmail.com>
2017-02-04 01:44:01 -0700

    Update t (_8(|) REBIT.pl

--- t (_8(|) REBIT.pl
+++ t (_8(|) REBIT.pl
@@ -14,7 +14,16 @@ foreach my $minion (@active) {
   my @response = up_MASTER($minion);
   my $old = "$minion".'_MASTER';
   open($mfp, '<', $old);
-  
+  my @old = readline $mfp; chomp @old;
+  my $countold = @old; my $countresponse = @response;
+  my @new_master;
+  my %tmp = map{$_ => undef} @response;
+  foreach my $p (@old) { 
+	  if (exists $tmp{$p}) 
+      { next; }
+	  else 
+      { push @new_master, $p; }
+  }
 }
 sub counter { # list $minion_dump 
   my ($mininon) = @_;
