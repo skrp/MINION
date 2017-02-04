334320c778a3da0900e0aa2e7360d9aafce0256c
Prince of Archives <rich.tijerina@gmail.com>
2017-02-04 00:49:41 -0700

    Create ( C c >'_' PING_PID.pl

---
+++ ( C c >'_' PING_PID.pl
@@ -0,0 +1,12 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+##############################
+# PING_PID - test minion state
+#  ( C c >'_'  ---skrp of MKRX
+# default list minnion | test
+# if argv test those only
+
+sub ping {
+  my ($proc) = @_;
+  
+}
