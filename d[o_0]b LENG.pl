c3ace12a435c5dc388ed27f6be5fd9ab1c7b2185
Prince of Archives <rich.tijerina@gmail.com>
2017-02-03 23:56:52 -0700

    Create d[o_0]b LENG.pl

---
+++ d[o_0]b LENG.pl
@@ -0,0 +1,21 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+use Proc::Daemon;
+use Term::ANSIColors;
+##########################
+# LENG - minion log parser
+#  d[o_0]b ---skrp of MKRX
+#  -log -alerts
+my %color_scheme = (
+  
+);
+
+sub map { #MAP_OUTPUT
+
+}
+sub log { #STDOUT
+
+}
+sub alert { #STDERR
+
+}
