f90f08384ee97336c074f761452b90ce664258f3
Prince of Archives <rich.tijerina@gmail.com>
2017-02-04 00:57:57 -0700

    Update and rename (_8(|) REBIT.pl to t (_8(|) REBIT.pl

--- t (_8(|) REBIT.pl
+++ t (_8(|) REBIT.pl
@@ -1,5 +1,5 @@
 #!/usr/local/bin/perl
-##########################
+################### TOOL #
 # REBIT - iteration parser
 #   (_8(|) ---skrp of MKRX
 # - cur_iter - init_length - update_MASTER - cat_init
