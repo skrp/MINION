9943761394216d351ab6dde9e726739810f6b6f1
Prince of Archives <rich.tijerina@gmail.com>
2017-02-07 22:04:22 -0700

    Update t BURP.pl

--- t BURP.pl
+++ t BURP.pl
@@ -1,6 +1,14 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
 use MINION;
+use Term::ANSIColor;
 ########################
 # BURP - print dump size
 #
+my (@active) = @ARGV;
+foreach $minion (@active) {
+  my $minpath = "$minion/$minion".'_dump';
+  my @list = readdir($minpath);
+  foreach (@list) 
+    { print "$minion $_\n"; }
+}
