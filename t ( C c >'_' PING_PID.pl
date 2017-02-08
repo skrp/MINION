18c068f319f33e475bcac452c05b7e316661cd93
Prince of Archives <rich.tijerina@gmail.com>
2017-02-07 17:19:56 -0700

    Update t ( C c >'_' PING_PID.pl

--- t ( C c >'_' PING_PID.pl
+++ t ( C c >'_' PING_PID.pl
@@ -5,15 +5,19 @@ use strict; use warnings;
 #  ( C c >'_'  ---skrp of MKRX
 # default list minnion | test
 # if argv test those only
-my (@minions) = @ARGV;
-foreach (@minions) 
-  { my $return = ping($_); print "$return\n"; }
+my @minions = readdir("MINION/");
+my @up;
+foreach my $minion (@minions) { 
+  my $return = ping($minion); print "$return\n"; 
+  push @up, $minion if defined $return;
+}
+print "@up\n";
 # SUB ########################
 sub ping {
   my ($minion) = @_;
   my $minon_path = "MINION/$minion/$minion".'_PID';
   open($mfp, '<', $minion_path);
-  my $pid = readline $mfp; chomp $pid;
+  my $pid = readline $mfp; chomp $pid; close $mfp;
   my @all = system(`ps $pid`);
   my $response = $all[1]; 
   return $response;
