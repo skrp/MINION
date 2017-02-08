148c75d8056c02e422680d2c573814117c52a4f2
Prince of Archives <rich.tijerina@gmail.com>
2017-02-07 22:15:25 -0700

    Update t CHIRP.pl

--- t CHIRP.pl
+++ t CHIRP.pl
@@ -5,9 +5,9 @@ use Term::ANSIColor;
 # CHIRP - print STDOUT
 my (@active) = @ARGV;
 foreach $minion (@active) {
-  my $file = "$minion/$minion".'_DEBUG';
-  open(my $dfh, '<', $file);
-  my @contents = readline $dfh; chomp @contents; close $dfh;
+  my $file = "$minion/$minion".'_LOG';
+  open(my $lfh, '<', $file);
+  my @contents = readline $lfh; chomp @contents; close $lfh;
   foreach (@contents)
     { print color('magenta'); print "$minion "; print color('bold yellow'); print "$_\n"; }
-  
+}
