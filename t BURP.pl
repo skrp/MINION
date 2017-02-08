8aa75a12c8650198f0f06beee38b52ac7d7051e7
Prince of Archives <rich.tijerina@gmail.com>
2017-02-07 22:07:21 -0700

    Update t BURP.pl

--- t BURP.pl
+++ t BURP.pl
@@ -9,6 +9,9 @@ my (@active) = @ARGV;
 foreach $minion (@active) {
   my $minpath = "$minion/$minion".'_dump';
   my @list = readdir($minpath);
-  foreach (@list) 
-    { print "$minion $_\n"; }
+  foreach (@list) { 
+    print color('magenta');
+    print "$minion "; 
+    print color('bold yellow');
+    print "$_\n"; }
 }
