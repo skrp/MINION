2d88a503823aad79fdc24e051d701dee14874e8f
Prince of Archives <rich.tijerina@gmail.com>
2017-03-30 18:54:13 -0600

    Update and rename s d[o_0]b LENG.pl to LENG.pl

--- LENG.pl
+++ LENG.pl
@@ -6,6 +6,7 @@ use Term::ANSIColors;
 # LENG - minion log parser
 #  d[o_0]b ---skrp of MKRX
 #  -log -alerts
+
 my $conf_file = ".MIINION_conf";
 open(my $fp, '<', $conf_file);
 my %conf = readline $fp; # does this work
