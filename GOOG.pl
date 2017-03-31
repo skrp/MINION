2cde7a3c67fc1ff6c94489c9eec1c65e790264e7
Prince of Archives <rich.tijerina@gmail.com>
2017-03-30 18:53:46 -0600

    Update and rename s `o_0 GOOG.pl to GOOG.pl

--- GOOG.pl
+++ GOOG.pl
@@ -6,15 +6,18 @@ use WWW::Mechanize;
 ################## SUMMONS #
 # GOOG - imgur viral scraper
 #    `o_0    ---skrp of MKRX
-# SETUP ####################
-my $init = 'GOOG_INIT';
-my $dump = 'GOOG_dump';
-# DAEMONIZE ################
+# SETUP ###############################
+my $work = 'MINION/' my $dump = 'dump';
+my $state = 'STATE'; my $debug = 'DEBUG';
+my $log = 'LOG'; my $pid = 'PID';
+my $que = 'QUE'; my $clean = 'CLEAN'
+my $pause = 'PAUSE'; my $shutdown = 'SHUT';
+# DAEMONIZE ##########################
 my $daemon = Proc::Daemon->new(
-	work_dir => '/OPN/MINION/MINION/GOOG',
-	child_STDOUT => 'GOOG_LOG',
-	child_STDERR => '+>>GOOG_DEBUG',
-	pid_file => 'GOOG_PID',
+    work_dir     => $work,
+    child_STDOUT => $log,
+    child_STDERR => +>>$debug,
+    pid_file     => $pid,
 );
 $daemon->Init();
 # PROC #####################
