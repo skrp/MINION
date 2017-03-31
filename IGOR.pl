482aa82d018ae23dba6a458cc906b5d23b541ba1
Prince of Archives <rich.tijerina@gmail.com>
2017-03-30 18:48:36 -0600

    Update IGOR.pl

--- IGOR.pl
+++ IGOR.pl
@@ -7,15 +7,20 @@ use File::Find::Rule;
 #   ($.$)  ---skrp of MKRX
 # start - stop - pause - ping_pid
 my @cmds = qw(pause stat errchk dive countoff);
-# DAEMONIZE ##############
-$daemon = Proc::Daemon->new(
-    work_dir     => '/MINION/IGOR',
-    child_STDOUT => 'LOG',
-    child_STDERR => '+>>DEBUG',
-    pid_file     => 'PID',
+# SETUP ###############################
+my $work = 'MINION/' my $dump = 'dump';
+my $state = 'STATE'; my $debug = 'DEBUG';
+my $log = 'LOG'; my $pid = 'PID';
+my $que = 'QUE'; my $clean = 'CLEAN'
+my $pause = 'PAUSE'; my $shutdown = 'SHUT';
+# DAEMONIZE ##########################
+my $daemon = Proc::Daemon->new(
+    work_dir     => $work,
+    child_STDOUT => $log,
+    child_STDERR => +>>$debug,
+    pid_file     => $pid,
 );
 $daemon->Init();
-# 
 # SUB ####################
 sub ping_pid { 
   my $target = '/MINION/';
