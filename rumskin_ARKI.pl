f8976a94d63c943c00a7957251a17b1d793b5896
Prince of Archives <rich.tijerina@gmail.com>
2017-05-03 16:33:23 -0700

    Create rumskin_ARKI.pl

---
+++ rumskin_ARKI.pl
@@ -0,0 +1,36 @@
+#!/usr/local/bin/perl
+use strict; use warnings; $|=1; $0='ARKI';
+use Proc::Daemon;
+use LWP::UserAgent;
+# BIRTH ##################
+SOUL
+fork
+daemonize
+# INHERIT ################
+PID_SURNAME
+AGE
+DIARY
+SLEEP
+SUICIDE
+# COMM #################
+LOG
+TALK
+HEAR
+# BEGIN ################
+INIT
+FACE
+# WORK #################
+QUE
+STATE
+DUMP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+AGE - hour measurements
+every loop check hour
+  create STATE
+  DIARY
+    post
+    feed
+    respond
+  SUICIDE?
+  SLEEP?
+SOUL
