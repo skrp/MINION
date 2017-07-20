070e195812d683a27b9bdde036e29b55c39f97de
Prince of Archives <rich.tijerina@gmail.com>
2017-07-20 12:57:34 -0700

    Update ARKI_current.pl

--- ARKI_current.pl
+++ ARKI_current.pl
@@ -162,21 +162,24 @@ sub arki
 	my ($i) = @_;
 	sleep 1;
 	my $ua = uagent();
+	my $file = "$dump"."$i.pdf";
+	my $mfile = "$dump"."$i".'_meta.xml';
 	my $url = "$base/$i/$i.pdf";
 	my $murl = "$base/$i/$i".'_meta.xml';	
-	my $resp = $ua->get($url, ':content_file'=>"$dump/$i.pdf"); 
-	my $mresp = $ua->get($murl, ':content_file'=>"$dump/$i".'_meta_xml');
-	if (-f "$dump/$i.pdf") 
+	my $resp = $ua->get($url, ':content_file'=>$file); 
+	my $mresp = $ua->get($murl, ':content_file'=>$mfile);
+	if (-f $file) 
 		{ print $Lfh "YAY $i\n"; }
 	else
 	{ 
 		my $eresp = $ua->get("$base/$i", ':content_file'=>"$dump/tmp");
 		my $redo = `grep pdf $dump/tmp | sed 's?</a>.*??' | sed 's/.*>//'`;
-		my $rresp = $ua->get("$base/$i/$redo", ':content_file'=>"$dump/$i.pdf");
-		if (-f "$dump/$i.pdf") 
+		my $rresp = $ua->get("$base/$i/$redo", ':content_file'=>$file);
+		if (-f $file) 
 			{ print $Lfh "YAY $i\n"; }
 		else 
 			{ print $Lfh "FAIL $i\n"; next; }
 	}
-	XS("$dump"."$i.pdf") && unlink("$dump"."$i.pdf");
+	XS($file) && unlink($file);
+	XS($mfile) && unlink($mfile);
 }
